f43ecfabb961b300d92172dccff059f9c34315a9
Erik Bravo <125538399+erik180903@users.noreply.github.com>
2026-03-12 14:24:48 +0000

    Permisos arreglados

---
+++ ExpoBin.py
@@ -0,0 +1,18 @@
+def exponenciacion_binaria(M, e, n):
+    bits = bin(e)[2:]
+    k = len(bits)
+    if bits[0] == '1':
+        C = M % n
+    else:
+        C = 1
+    for i in range(1, k):
+        ei = bits[i]
+        C = (C ** 2) % n
+        if ei == '1':
+            C = (C * M) % n
+    return C
+M = int(sys.argv[1])
+e = int(sys.argv[2])
+n = int(sys.argv[3])
+
+print(exponenciacion_binaria(M, e, n))
---
+++ expobin.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python ExpoBin.py
