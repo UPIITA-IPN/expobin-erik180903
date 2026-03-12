bada5881eaaaedc59264d93f42aefbc074c2f2e9
Erik Bravo <125538399+erik180903@users.noreply.github.com>
2026-03-12 08:04:22 -0600

    Fix argument handling in exponenciacion_binaria

--- ExponBin.py
+++ ExponBin.py
@@ -11,8 +11,9 @@ def exponenciacion_binaria(M, e, n):
         if ei == '1':
             C = (C * M) % n
     return C
-M = int(sys.stdin.read().strip())
-e = int(sys.argv[1])
-n = int(sys.argv[2])
+M = int(sys.argv[1])
+e = int(sys.argv[2])
+n = int(sys.argv[3])
 
 print(exponenciacion_binaria(M, e, n))
+
